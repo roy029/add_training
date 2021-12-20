70b89b9434a73d4f476f255e57a7b69d83479fad
kajiura <79959901+roy029@users.noreply.github.com>
2021-12-20 13:18:50 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# T5ライブラリ
+pip install t5[gcp]==0.7.1
+pip install tensorflow-gpu
+git clone https://github.com/google-research/text-to-text-transfer-transformer
+git clone https://github.com/google-research/multilingual-t5.git
+
+pip install datasets
+pip install flax
+pip install transformers

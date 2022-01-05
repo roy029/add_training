56785b67ae8ecdad336df36af8a45925f60bd709
kajiura <79959901+roy029@users.noreply.github.com>
2022-01-05 23:13:36 +0900

    Add files via upload
    ABCIでバッチ処理するためのスクリプト

---
+++ ABCI_mT5_add/batch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#$-l rt_F=5
+#$-j y
+#$-cwd
+
+source /etc/profile.d/modules.sh
+module load python/3.6/3.6.5
+module load cuda/9.2/9.2.88.1
+
+#!/bin/bash -e
+pip3 install --user --upgrade pip
+pip3 install -r requirements.txt
+
+#!/bin/bash
+./a.out

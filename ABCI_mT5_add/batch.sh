e7bf650e68711263fd99a93c0bca07b6e140cd07
acd13734km <acd13734km@es1.abci.local>
2022-01-07 20:11:42 +0900

    現状

--- ABCI_mT5_add/batch.sh
+++ ABCI_mT5_add/batch.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#$-l rt_F=5
+
+#$ -l rt_G.small=1
+#$ -l h_rt=24:00:00
 #$-j y
+#$-m b
+#$-m a
+#$-m e
 #$-cwd
+
 
 source /etc/profile.d/modules.sh
 module load python/3.6/3.6.5
@@ -11,7 +17,9 @@ module load cuda/9.2/9.2.88.1
 #!/bin/bash -e
 pip3 install --user --upgrade pip
 pip3 install -r requirements.txt
-python3 mask_T5.py output_small.txt
+pip3 install slackweb
+python3 mask_T5.py conala-mined.txt
+python3 slack.py
 
 #!/bin/bash
 # ./a.out
